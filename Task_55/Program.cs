8ed29c65aa2429b55c591b81a913a07c8b6508fc
Margarita <margarita_anis@bk.ru>
2022-06-23 20:30:10 +0500

    Add project Task_55

---
+++ Task_55/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rndNum.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    Console.WriteLine("Двумерный массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SwitchRowsToColumns(int [,] array)
+{
+    int[,] newArr = array;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArr[i, j] = array[j, i];
+        }
+    }
+    return newArr;
+}
+int[,] arr = GetArray(3, 3);
+PrintArray(arr);
+
+Console.WriteLine();
+
+int[,] newArr = SwitchRowsToColumns(arr);
+PrintArray(newArr);
