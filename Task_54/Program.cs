fefe0df5fedb4efd09a8a469ad282ec1890860cb
Margarita <margarita_anis@bk.ru>
2022-07-02 16:28:35 +0500

    Add HOMEWORK Task_54

---
+++ Task_54/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Write("Задайте кол-во строк двумерного массива: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Задайте кол-во столбцов двумерного массива: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int Rows, int Columns)
+{
+    int[,] array = new int[row, column];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rndNum.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    Console.WriteLine("Двумерный массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] BubbleSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - j - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] arr = GetArray(row, column);
+PrintArray(arr);
+
+Console.WriteLine();
+
+int[,] newArray = BubbleSort(arr);
+PrintArray(newArray);
